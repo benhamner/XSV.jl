31126f2ad266ae48ccff9fc7cfd9e4305e335b28
Ben Hamner <ben@benhamner.com>
2014-11-16 18:10:32 -0800

    added unit tests

--- test/readxsv.jl
+++ test/readxsv.jl
@@ -15,5 +15,8 @@ using XSV
 @test readxsvs("1;2;3\n4;5;6", delimiter=';')[1][3]=="3"
 @test readxsvs("1;2;3\n4;5;6", delimiter=';')[2][2]=="5"
 
-@test readxsvs("\"1\";2;\"3\"\n4;5;6", delimiter=';')[1][3]=="3"
+@test readxsvs("\"1\";2;\"3\"\n4;5;6",     delimiter=';')[1][3]=="3"
 @test readxsvs("1;2;3\n\"4\";\"5\";\"6\"", delimiter=';')[2][2]=="5"
+
+@test readxsvs("B1B;2;B3B\n4;5;6",   delimiter=';', quotechar='B')[1][3]=="3"
+@test readxsvs("1;2;3\nB4B;B5B;B6B", delimiter=';', quotechar='B')[2][2]=="5"
